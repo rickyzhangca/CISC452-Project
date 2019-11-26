731140d430852ca10ab7e8080abce7b258530fc3
Ricky Zhang <ricky.zhang@queensu.ca>
2019-11-26 18:33:20 -0500

    predict is now working

--- yolo_video.py
+++ yolo_video.py
@@ -4,16 +4,17 @@ from yolo import YOLO, detect_video
 from PIL import Image
 
 def detect_img(yolo):
-    while True:
-        img = input('Input image filename:')
-        try:
-            image = Image.open(img)
-        except:
-            print('Open Error! Try again!')
-            continue
-        else:
-            r_image = yolo.detect_image(image)
-            r_image.show()
+    #while True:
+        # img = input('Input image filename:')
+    img = 'test.jpg'
+    try:
+        image = Image.open(img)
+    except:
+        print('Open Error! Try again!')
+        #continue
+    else:
+        r_image = yolo.detect_image(image)
+        r_image.show()
     yolo.close_session()
 
 FLAGS = None
